f7327d9cf38792baf22b7c0d09fd1c849e9722e6
hafiz <muhammad.mateen@amroodlabs.com>
2025-10-02 21:08:28 +0500

    ADDED: change password button added with modal

---
+++ app/api/auth/change-password/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+import clientPromise from "@/lib/mongodb";
+import { PassThrough } from "stream";
+
+export async function POST(req: NextRequest) {
+  try {
+    const[currentPassword, newPassword] = await req.json();
+
+    // Getting token from cookies [receiving request]
+    const token = req.cookies.get("token")?.value;
+    
+    // Authenticating user
+    if(!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+    const userId = decoded.userId;
+
+    // Finding user in db using userId
+    const client = await clientPromise;
+    const db = client.db("auth-module");
+
+    const user = await db.collection("users").findOne({_id: userId});
+    if(!user) {
+      return NextResponse.json({error: "User not found"}, {status: 404});
+    }
+
+    // Verify current password
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if(!isMatch) {
+      return NextResponse.json({ error: "Incorrect current password!" }, { status: 400 });
+    }
+
+    // Validate new password
+    if (newPassword.length < 8 || !/[A-Z]/.test(newPassword) || !/\d/.test(newPassword)) {
+      return NextResponse.json({ error: "Weak password" }, { status: 400 });
+    }
+
+    // Update password in DB  
+    const hashed = await bcrypt.compare(currentPassword, newPassword);
+    await user.db.collection("users").updatedOne({_id: userId}, {$set: {password: hashed}});
+
+    // Invalidate old session
+    const res = NextResponse.json({ message: "Password updated successsfully. Please login again."}, { status: 200} );
+    res.cookies.set("token", "", {
+      httpOnly: true,
+      sameSite: "strict",
+      expires: new Date(0),
+      maxAge: 0,
+    })
+    return res;
+  } catch (err:any) {
+    return NextResponse.json({error: err.message || "Server error"}, {status: 500});
+  }
+}
---
+++ components/modals/ChangePasswordModal.tsx
@@ -0,0 +1,155 @@
+"use client"
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { validatePassword } from "@/utils/validators";
+import { ChangePasswordModalProps } from "../../utils/types"
+
+export default function ChangePasswordModal({ isOpen, onClose }: ChangePasswordModalProps) {
+  const[currentPassword, setCurrentPassword] = useState("");
+  const[newPassword, setNewPassword] = useState("");
+  const[confirmPassword, setConfirmPassword] = useState("");
+  const[loading, setLoading] = useState(false);
+  const[errors, setErrors] = useState<{currentPassword?:string, newPassword?:string, confirmPassword?:string, formError?:string}>({});
+  const router = useRouter();
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.name === "current-password") {
+      setCurrentPassword(e.target.value);
+    } else if(e.target.name === "new-password") {
+      setNewPassword(e.target.value);
+    } else if(e.target.name === "confirm-new-password") {
+      setConfirmPassword(e.target.value);
+    }
+  }
+
+  const handleBlur = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const validationError = validatePassword(e.target.value, true); //calling strict check for new password
+    setErrors((prev) => ({...prev, [e.target.name]: validationError}));
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // validation again on submit not just on blur
+    const currentPasswordError = validatePassword(currentPassword, true);
+    const newPasswordError = validatePassword(newPassword, true);
+    const confirmPasswordError = validatePassword(confirmPassword, true);
+
+    if(currentPasswordError || newPasswordError || confirmPasswordError) {
+      setErrors({currentPassword: currentPasswordError, newPassword: newPasswordError, confirmPassword: confirmPasswordError});
+      return;
+    }
+
+    if(newPassword !== confirmPassword) {
+      setErrors((prev) => ({ ...prev, formError: "Passwords do not match"}))
+      return;
+    }
+    
+    setErrors({});
+    // setErrors({ email: "", userName: "", password: "" });
+
+    try {
+      setLoading(true);
+
+      const response = await fetch("/api/auth/change-password", {
+        method: "POST",
+        headers: { "Content-type": "application/json"},
+        body: JSON.stringify({ currentPassword, newPassword }),
+        // credentials: "include" // No need in signup page bcz cookies are not setting in signup page
+      })
+
+      if(!response.ok) {
+        const errData = await response.json();
+        throw new Error(errData.error || "Failed to change password!");
+      }
+
+      //On sccess
+      console.log("Password Changed: ", {currentPassword}, "->", {newPassword});
+      alert("Password changed successfully, Please login again.")
+      onClose();
+      const data = await response.json();
+      console.log("Password changed successfull:", data);
+
+      router.push("/login");
+
+    } catch(err:any) {
+      // backend error message
+      setErrors((prev) => ({ ...prev, formError: err.message}))
+    } finally {
+      setLoading(false);
+    }
+
+  }
+
+  if(!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm">
+        <h2 className="text-xl font-semibold mb-4">Change Password</h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <label>Current Password</label>
+          <input 
+          name="currentPassword"
+          type="password"
+          value={currentPassword}
+          placeholder="Current password"
+          autoComplete="off"
+          required
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className="w-full border border-gray-300 rounded px-3 py-2 focus-visible:ring-gray-900"
+          />
+          {errors.currentPassword && <p className="text-red-500 text-sm">{errors.currentPassword}</p>}
+
+          <label>New Password</label>
+          <input
+          name="newPassword"
+          type="password"
+          value={newPassword}
+          placeholder="New password"
+          autoComplete="new-password"
+          required
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className="w-full border border-gray-300 rounded px-3 py-2 focus-visible:ring-gray-900"
+          />
+          {errors.newPassword && <p className="text-red-500 text-sm">{errors.newPassword}</p>}
+
+          <label>Confirm New Password</label>
+          <input 
+          name="confirmPassword"
+          type="password"
+          value={confirmPassword}
+          placeholder="Confirm password"
+          autoComplete="new-password"
+          required
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className="w-full border border-gray-300 rounded px-3 py-2 focus-visible:ring-gray-900"
+          />
+          {errors.confirmPassword && <p className="text-red-500 text-sm">{errors.confirmPassword}</p>}
+
+          <button
+            type="button"
+            onClick={onClose}
+            className="w-full bg-gray-400 text-white rounded py-2 font-semibold cursor-pointer hover:bg-gray-600"
+          >
+            Cancel
+          </button>
+          
+          <button
+            type="submit"
+            disabled={loading || Boolean(errors.currentPassword) || Boolean(errors.newPassword) || Boolean(errors.confirmPassword)}
+            className="w-full bg-blue-600 text-white rounded py-2 font-semibold cursor-pointer hover:bg-gray-700"
+          >
+            {loading? "Changing..." : "Change Password"}
+          </button>
+          {errors.formError && <p className="text-red-500 text-sm">{errors.formError}</p>}
+        </form>
+      </div>
+    </div>
+  )
+}

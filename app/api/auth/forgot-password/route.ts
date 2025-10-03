1eea0170cfc4339648da3daff6efcd1709d673d1
hafiz <muhammad.mateen@amroodlabs.com>
2025-10-03 14:47:27 +0500

    ADDED: forgot password

---
+++ app/api/auth/forgot-password/route.ts
@@ -0,0 +1,62 @@
+import { NextRequest, NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
+// import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email } = await req.json();
+
+    // Validate email format
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return NextResponse.json({ error: "Invalid email" }, { status: 400 });
+    }
+
+    // Finding user in db using userId
+    const client = await clientPromise;
+    const db = client.db("auth-module");
+    const user = await db.collection("users").findOne({email});
+
+    if(!user) {
+      return NextResponse.json({message: "If user exists, a reset link has been sent"}, {status: 200});
+    }
+
+    // Generate reset token
+    // const resetToken = crypto.randomBytes(32).toString("hex");
+    // const expires = new Date(Date.now() + 1000 * 60 * 15);
+
+    // await db.collection("resetTokens").insertOne({
+    //   userId: user._id,
+    //   token: resetToken,
+    //   expires,
+    // })
+
+    const resetToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET!,
+      { expiresIn: "15m" }
+    );
+
+    const resetUrl = `${process.env.AUTH_MODULE_APP_URL}/reset-password?token=${resetToken}`;
+
+    await resend.emails.send({
+      from: '"Auth Module" <no-reply@auth-module.com>',
+      to: email,
+      subject: "Password Reset Request",
+      html: `
+        <p>You requested a password reset</p>
+        <p>Click here to reset your password: <a href="${resetUrl}">${resetUrl}</a></p>
+        <p>This link will expire in 15 minutes.</p>
+      `
+    });
+
+    console.log("link sent on:", email, "token", resetToken);
+        
+    return NextResponse.json({message: "If user exists, a reset link has been sent"}, {status: 200});
+  } catch (err:any) {
+    return NextResponse.json({error: err.message || "Server error"}, {status: 500});
+  }
+}
---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,105 @@
+"use client"
+
+import { useState } from "react";
+import { redirect, useRouter } from "next/navigation";
+import { validateEmail } from "@/utils/validators";
+
+export default function ForgotPassword() {
+  const[email, setEmail] = useState("");
+  const[loading, setLoading] = useState(false);
+  const[errors, setErrors] = useState<{email?:string, formError?:string}>({});
+  const router = useRouter();
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }
+
+  const handleBlur = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const validationError = validateEmail(e.target.value);
+    setErrors((prev) => ({...prev, email: validationError}));
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // validation again on submit not just on blur
+    const emailError = validateEmail(email);
+
+    if (emailError) {
+      setErrors({ email: emailError})
+      return;
+    }
+    
+    setErrors({});
+
+    try {
+      setLoading(true);
+
+      const response = await fetch("/api/auth/forgot-password", {
+        method: "POST",
+        headers: { "Content-type": "application/json"},
+        body: JSON.stringify({ email }),
+        // credentials: "include" // No need in signup page bcz cookies are not setting in signup page
+      })
+
+      if(!response.ok) {
+        const errData = await response.json();
+        throw new Error(errData.error || "Invalid Email!");
+      }
+
+      //On sccess
+      console.log("One time verification code link sent on email: ", {email});
+      alert("One time email verification code is just sent to your email.")
+
+      router.push("/reset-password");
+
+    } catch(err:any) {
+      // backend error message
+      setErrors((prev) => ({ ...prev, formError: err.message}))
+    } finally {
+      setLoading(false);
+    }
+
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm">
+        <h2 className="text-xl text-center font-semibold mb-4">Forgot Password</h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <label>Enter your Email</label>
+          <input
+          name="email"
+          type="email"
+          value={email}
+          placeholder="Email"
+          autoComplete="email"
+          required
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className="w-full border border-gray-300 rounded px-3 py-2 focus-visible:ring-gray-900"
+          />
+          {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+
+          <button
+            type="button"
+            onClick={router.back}
+            className="w-full bg-gray-400 text-white rounded py-2 font-semibold cursor-pointer hover:bg-gray-600 transition"
+          >
+            Cancel
+          </button>
+          
+          <button
+            type="submit"
+            disabled={loading || Boolean(errors.email)}
+            className="w-full bg-blue-600 text-white rounded py-2 font-semibold cursor-pointer hover:bg-blue-700 transition"
+          >
+            {loading? "Sending..." : "Send Code link"}
+          </button>
+          {errors.formError && <p className="text-red-500 text-sm">{errors.formError}</p>}
+        </form>
+      </div>
+    </div>
+  )
+}

d201bd7c980e4eac068051ecd83776ab74d4efe9
hafiz <muhammad.mateen@amroodlabs.com>
2025-10-06 14:19:47 +0500

    UPDATE: Redirected user to login page on cancel action from reset form

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import ChangePasswordModal from "@/components/modals/ChangePasswordModal";
@@ -8,14 +7,19 @@ export default function ResetPasswordPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token"); // get ?token=... from URL
   const [isOpen, setIsOpen] = useState(true);
+  const router = useRouter();
 
-  const handleClose = () => setIsOpen(false);
+  const handleClose = () => {
+    setIsOpen(false);
+    router.push("/login");
+  }
 
   return (
     <div>
       <ChangePasswordModal
         isOpen={isOpen}
         onClose={handleClose}
+        isResetFlow={true}
         token={token} // pass token if your modal needs it
       />
     </div>

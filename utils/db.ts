a4181aac9070fb6f9a071b1d54cca6c8e2d4f4a5
hafiz <muhammad.mateen@amroodlabs.com>
2025-10-14 19:04:21 +0500

    ADDED: MongoDB connection utility for posts integration

---
+++ utils/db.ts
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+export default async function connectDB() {
+  if (mongoose.connection.readyState >= 1) return;
+  await mongoose.connect(process.env.MONGODB_URI as string);
+}

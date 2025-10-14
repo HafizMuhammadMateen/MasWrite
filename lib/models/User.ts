86b8d1cf688bf40aec7067288b4c3e238baf239b
hafiz <muhammad.mateen@amroodlabs.com>
2025-10-14 19:05:27 +0500

    UPDATED: moved User interface to types/user.ts

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -1,10 +1,17 @@
-import { ObjectId } from "mongodb";
+import { User } from "@/lib/types/user";
+import clientPromise from "@/lib/mongodb";
 
-export interface User {
-  _id?: ObjectId;
-  email: string;
-  userName: string;
-  password: string; // will store hashed password
-  createdAt?: Date;
-  updatedAt?: Date;
+export async function getUserCollection() {
+  const client = await clientPromise;
+  return client.db().collection<User>("users");
 }
+
+// import mongoose, { Schema, models } from "mongoose";
+
+// const userSchema = new Schema({
+//   email: { type: String, required: true },
+//   userName: { type: String, required: true },
+//   password: { type: String, required: true },
+// }, { timestamps: true });
+
+// export default models.User || mongoose.model("User", userSchema);

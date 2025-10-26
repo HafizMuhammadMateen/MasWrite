bbdae390a3fd6959a5b6df6d57516bc8bfe4c91a
Hafiz Muhammad Mateen <muhammadmateen546@gmail.com>
2025-10-26 12:49:17 +0500

    UPDATED: shoft view counter route from [slug] to [id] & added exception handing

--- app/api/blogs/[id]/view/route.ts
+++ app/api/blogs/[id]/view/route.ts
@@ -1,19 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/utils/db";
 import Blog from "@/lib/models/Blog";
-import { error } from "console";
 
-export async function POST(_: NextRequest, context: { params: Promise<{ slug: string }> }) {
-  await connectDB();
-  const { slug } = await context.params;
+export async function POST(_: NextRequest, context: { params: Promise<{ id: string }> }) {
+  const isDev = process.env.NODE_ENV === "development";
+  try {
+    await connectDB();
+    const { id } = await context.params;
 
-  const blog = await Blog.findOneAndUpdate(
-    { slug },
-    { $inc: { views: 1 } },
-    { new: true, projection: { views: 1 } } // return only views field
-  );
+    const blog = await Blog.findByIdAndUpdate(
+      id,
+      { $inc: { views: 1 } },
+      { new: true, projection: { views: 1 } } // return only views field
+    );
 
-  if (!blog) return error("Blog not found", 404);
+    if (!blog) return NextResponse.json({ message: "Blog not found" }, { status: 404 });
 
-  return NextResponse.json({ views: blog.views });
+    return NextResponse.json({ views: blog.views });
+  } catch (err) {
+    isDev && console.error("[POST API] Increment blog views error:", err);
+    return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+  }
 }
+
